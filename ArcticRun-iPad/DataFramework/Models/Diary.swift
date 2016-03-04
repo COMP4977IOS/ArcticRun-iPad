cab63b5f226b8aa82468e0604c318f067998c8f3
Anthony Smith <anthony@smith.lt>
2016-03-03 23:10:15 -0800

    Add initial data model for saving game elements

---
+++ ArcticRun-iPad/DataFramework/Models/Diary.swift
@@ -0,0 +1,52 @@
+//
+//  Diary.swift
+//  ArcticRun-iPad
+//
+//  Created by Anthony on 2016-03-03.
+//  Copyright © 2016 COMP 4977. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+public class Diary: Object {
+    
+    private dynamic var _crew:Crew!
+    private dynamic var _date:NSDate!
+    private dynamic var _text:String!
+    
+    convenience init(crew: Crew, date: NSDate, text:String) {
+        self.init()
+        self._crew = crew
+        self._date = date
+        self._text = text
+    }
+    
+    public var crew:Crew {
+        get {
+            return self._crew
+        }
+        set {
+            self._crew = newValue
+        }
+    }
+    
+    public var date:NSDate {
+        get {
+            return self._date
+        }
+        set {
+            self._date = newValue
+        }
+    }
+    
+    public var text:NSDate {
+        get {
+            return self.text
+        }
+        set {
+            self.text = newValue
+        }
+    }
+    
+}
---
+++ ArcticRun-iPad/DataFramework/Models/ItemType.swift
@@ -0,0 +1,30 @@
+//
+//  ItemType.swift
+//  ArcticRun-iPad
+//
+//  Created by Anthony on 2016-03-03.
+//  Copyright © 2016 COMP 4977. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+public class ItemType: Object {
+    
+    private dynamic var _name:String!
+    
+    convenience init(name:String) {
+        self.init()
+        self._name = name
+    }
+    
+    public var name:String {
+        get {
+            return self._name
+        }
+        set {
+            self._name = newValue
+        }
+    }
+    
+}
